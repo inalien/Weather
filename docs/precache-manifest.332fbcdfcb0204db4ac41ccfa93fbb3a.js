c59e9189c33669657e8a0c53a749c19be220e98c
Jhon <jhon.inalien@gmail.com>
2020-09-08 15:31:53 -0400

    API

---
+++ docs/precache-manifest.332fbcdfcb0204db4ac41ccfa93fbb3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800147ef7c6173b61cc2160ecff94787",
+    "url": "./index.html"
+  },
+  {
+    "revision": "514ae3c554f8f7879f25",
+    "url": "./static/js/2.2bfdb317.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2bfdb317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a14a16381c72f496f0a",
+    "url": "./static/js/main.476dde42.chunk.js"
+  },
+  {
+    "revision": "7185123dbfbf19a73c74",
+    "url": "./static/js/runtime-main.3def4396.js"
+  }
+]);
